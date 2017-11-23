9d99ae3e2c412ea439700b4ab6c56b64dced0025
Kristianengberg <krissoengberg@gmail.com>
2017-11-23 17:53:25 +0100

    Added what we did at school. Put in learning schedule for stochastic gradient descent# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	modified:   .idea/machinelearning_p5.iml#	modified:   .idea/misc.xml#	modified:   .idea/workspace.xml#	modified:   Basic Value Function.py#

--- Basic Value Function.py
+++ Basic Value Function.py
@@ -2,21 +2,28 @@ import random
 import gym
 
 
-amount_games = 200
+amount_games = 1000
 amount_of_steps = 200
 
 gamma = 0.95
-alpha = 0.95
+alpha = 0
+
+t0, t1 = 5, 50
+
 
 env = gym.make('CartPole-v0')
 env.reset()
 
 weight = []
 
+
 def init(how_many_weights):
     for i in range(how_many_weights):
         weight.append(random.uniform(0.1, how_many_weights))
     #print('initial weights', weight)
+
+def learning_schedule(t):
+    return t0 /(t+t1)
 
 def q_function(observation):
     #Q(s) = w1*f1(s) + w2*f2(s) + .. + wn*fn(s)
@@ -24,7 +31,7 @@ def q_function(observation):
 
     for i in range(len(weight)):
         #Q(s) = wi*fi(s)
-        q_value += weight[i] * observation[i]
+        q_value += weight[i] * f_function(observation)
 
     return q_value
 
@@ -46,17 +53,17 @@ def update_weights(old_obs, new_obs, reward):
 
     for i in range(len(weight)):
         weight[i] = weight[i] + alpha * delta_q(old_obs, new_obs, reward)*old_obs[i]
-    print('Updated weights', weight)
+    #print('Updated weights', weight)
 
 init(4)
 
-
-
-for _ in range(amount_games):
+overall_reward_final_games = 0
+hit_target = False
+for game in range(amount_games):
     overall_reward = 0
     observation = env.reset()
     for i in range(amount_of_steps):
-        env.render()
+        #env.render()
         q_value = q_function(observation=observation)
         #print('This is the Q Value', q_value)
         if q_value < 0:
@@ -69,11 +76,22 @@ for _ in range(amount_games):
         observation, reward, done, info = env.step(action)
         overall_reward += reward
 
+        alpha = learning_schedule(game * amount_of_steps + i)
+        if hit_target == False:
+            update_weights(old_obs=old_obseravtion, new_obs=observation, reward=reward)
 
-
-        update_weights(old_obs=old_obseravtion, new_obs=observation, reward=reward)
-
-
+        if game > 500:
+            overall_reward_final_games += reward
 
         if done: break
-    print(overall_reward)
+
+
+    #print(weight)
+    #print(overall_reward)
+
+print(overall_reward_final_games/500)
+sum_of_weight = 0
+for i in range(len(weight)):
+    sum_of_weight  += weight[i]
+
+print(sum_of_weight)
