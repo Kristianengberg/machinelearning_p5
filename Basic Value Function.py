244cdfa5b44c10773a906d0e8ebc9fe996b4f13f
Kristianengberg <krissoengberg@gmail.com>
2017-11-29 16:53:03 +0100

    trying to add Q function that evaluate Action

--- Basic Value Function.py
+++ Basic Value Function.py
@@ -1,8 +1,8 @@
 import random
 import gym
+import numpy as np
 
-
-amount_games = 1000
+amount_games = 5000
 amount_of_steps = 200
 
 gamma = 0.95
@@ -19,40 +19,45 @@ weight = []
 
 def init(how_many_weights):
     for i in range(how_many_weights):
-        weight.append(random.uniform(0.1, how_many_weights))
+        weight.append(0)
     #print('initial weights', weight)
 
 def learning_schedule(t):
     return t0 /(t+t1)
 
-def q_function(observation):
+def q_function(observation, action):
     #Q(s) = w1*f1(s) + w2*f2(s) + .. + wn*fn(s)
     q_value = 0
 
     for i in range(len(weight)):
+
         #Q(s) = wi*fi(s)
-        q_value += weight[i] * f_function(observation)
+        #q_value += weight[i] * f_function(observation[i], action)
+        q_value += weight[i] * (observation[i] * action)
 
     return q_value
 
 
-def f_function(observation):
-    f = 0
-    for i in range(len(observation)):
-       f += observation[i]
+def f_function(observation, action):
+    #f = 0
+
+    #for i in range(len(observation)):
+    f = observation + action
 
     return f
 
-def delta_q(old_obs, new_obs, reward):
-
-    delta_q_value = reward + gamma*(q_function(new_obs) - q_function(old_obs))
+def delta_q(old_obs, new_obs, reward, action):
+    if action == 1:
+        other_action = -1
+    else: other_action = 1
+    delta_q_value = reward + gamma*np.argmax([q_function(new_obs,action), q_function(new_obs,other_action)]) - q_function(old_obs,action)
 
     return delta_q_value
 
-def update_weights(old_obs, new_obs, reward):
+def update_weights(old_obs, new_obs, reward, action):
 
     for i in range(len(weight)):
-        weight[i] = weight[i] + alpha * delta_q(old_obs, new_obs, reward)*old_obs[i]
+        weight[i] = weight[i] + alpha * delta_q(old_obs, new_obs, reward, action)*(old_obs[i] * action)
     #print('Updated weights', weight)
 
 init(4)
@@ -63,10 +68,16 @@ for game in range(amount_games):
     overall_reward = 0
     observation = env.reset()
     for i in range(amount_of_steps):
+        #if game > 950:
         #env.render()
-        q_value = q_function(observation=observation)
+        if game == 0:
+            action = 0
+            q_action = -1
+
+        q_value = q_function(observation=observation, action=-1)
+        q_value1 = q_function(observation=observation, action=1)
         #print('This is the Q Value', q_value)
-        if q_value < 0:
+        if q_value > q_value1:
             action = 0
         else:
             action = 1
@@ -78,9 +89,9 @@ for game in range(amount_games):
 
         alpha = learning_schedule(game * amount_of_steps + i)
         if hit_target == False:
-            update_weights(old_obs=old_obseravtion, new_obs=observation, reward=reward)
+            update_weights(old_obs=old_obseravtion, new_obs=observation, reward=reward, action=action)
 
-        if game > 500:
+        if game > (amount_games - 500):
             overall_reward_final_games += reward
 
         if done: break
